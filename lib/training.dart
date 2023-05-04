48b45c2fdd5e48ff0735cecc441c4537cc647794
rmnsfx <rmnsfx@users.noreply.github.com>
2023-05-04 17:11:34 +0300

    get collection map from db

--- lib/training.dart
+++ lib/training.dart
@@ -6,7 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
-final ref = FirebaseFirestore.instance.collection("Test");
+final ref = FirebaseFirestore.instance.collection("user1");
 
 class TrainingPage extends StatelessWidget {
   const TrainingPage({super.key});
@@ -16,55 +16,86 @@ class TrainingPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Тренировки"),
+        title: const Text("Тренировки"),
         backgroundColor: Colors.indigo.shade100,
       ),
       body: StreamBuilder(
-        stream: ref.snapshots(),
+        stream: FirebaseFirestore.instance.collection("user1").snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot>snapshot) {
-          return ListView.builder(
-              itemCount: day.length,
-              itemBuilder: (BuildContext context, index) {
-                return Card(
-                    child: ExpansionTile(
-                      leading: Icon(iconsTrain[0]),
-                      //trailing: Icon(Icons.keyboard_arrow_right),
-                      title:  Text('${day[index]}'),
-                      //subtitle: Text('${time[index]}'),
-                      children: <Widget>[
+          if(snapshot.hasData) {
 
-                        if( index == 0 )
-                          for (var item in trainingMonday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 1 )
-                          for (var item in trainingTuesday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 2 )
-                          for (var item in trainingWednesday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 3 )
-                          for (var item in trainingThursday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 4 )
-                          for (var item in trainingFriday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 5 )
-                          for (var item in trainingSaturday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                        if( index == 6 )
-                          for (var item in trainingSunday)
-                            ListTile(title: Text(item.name), subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
-                      ],
-                    )
+            final List<DocumentSnapshot> documents = snapshot.data!.docs;
+            final entryList = documents[0]['mon'].entries.toList();
 
-                );
-              }
-          );
+            return ListView.builder(
+                itemCount: day.length,
+                itemBuilder: (BuildContext context, index) {
+                  return Card(
+                      child: ExpansionTile(
+                        leading: Icon(iconsTrain[0]),
+                        //trailing: Icon(Icons.keyboard_arrow_right),
+                        title: Text(day[index]),
+                        //subtitle: Text('${time[index]}'),
+                        children: <Widget>[
+
+                          if( index == 0 )
+                            for (var item in trainingMonday)
+                              ListTile(
+
+                                title: Text( entryList[0].key ),
+                                subtitle: Text("Вес: ${item.weight}${item.units}; Подходов: ${item.touch}; Повторов: ${item.quantity}"),),
+
+                          if( index == 1 )
+                            for (var item in trainingTuesday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                          if( index == 2 )
+                            for (var item in trainingWednesday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                          if( index == 3 )
+                            for (var item in trainingThursday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                          if( index == 4 )
+                            for (var item in trainingFriday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                          if( index == 5 )
+                            for (var item in trainingSaturday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                          if( index == 6 )
+                            for (var item in trainingSunday)
+                              ListTile(title: Text(item.name), subtitle: Text(
+                                  "Вес: ${item.weight}${item
+                                      .units}; Подходов: ${item
+                                      .touch}; Повторов: ${item.quantity}"),),
+                        ],
+                      )
+
+                  );
+                }
+            );
+          }
+          else {
+            return const CircularProgressIndicator();
+          }
         }
       ),
 
     );
-  }
+    }
 }
 
 
